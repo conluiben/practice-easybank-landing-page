c4a5a84fcd6223600657d8db319e10f36e6b7856
CoreData Research <cdresearch@Luiz---CoreDatas-MacBook-Air.local>
2025-06-09 07:43:11 +0800

    feat: benefits section for mobile
    - added benefits section for mobile screens
    - rewrote descriptive text attributes as custom class (also applied to Hero in previous commit)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Benefits from "./components/Benefits";
 
 function App() {
   return (
     <div className="">
       <Navbar className="h-[60px] md:h-[80px] z-5" />
       <Hero className="mt-[60px] md:mt-[80px]" />
+      <Benefits />
     </div>
   );
 }
---
+++ src/components/Benefits.jsx
@@ -0,0 +1,58 @@
+import iconOnline from "../assets/icon-online.svg";
+import iconBudgeting from "../assets/icon-budgeting.svg";
+import iconOnboarding from "../assets/icon-onboarding.svg";
+import iconApi from "../assets/icon-api.svg";
+
+const listOfBenefits = [
+  {
+    heading: "Online Banking",
+    description:
+      "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+    image: iconOnline,
+  },
+  {
+    heading: "Simple Budgeting",
+    description:
+      "See exactly where your money goes each month. Receive notifications when you're close to your hitting limits.",
+    image: iconBudgeting,
+  },
+  {
+    heading: "Fast Onboarding",
+    description:
+      "We don't do branches. Open your account in minutes online and start taking control of your finances right away",
+    image: iconOnboarding,
+  },
+  {
+    heading: "Open API",
+    description:
+      "Manage your savings, investments, pension and much more from one account. Tracking your money has never been easier.",
+    image: iconApi,
+  },
+];
+
+const Benefits = () => {
+  return (
+    <div className="">
+      <div className="w-full max-w-[1200px] py-16 px-8 lg:px-16 lg:py-32 text-center">
+        <h2 className="text-3xl font-extralight mb-2">Why choose Easybank?</h2>
+        <p className="descriptive-text mb-16">
+          We leverage Open Banking to turn you bank account into your financial
+          hub. Control your finances like never before.
+        </p>
+        <div className="flex flex-col gap-8">
+          {listOfBenefits.map((aBenefit) => {
+            return (
+              <div className="flex flex-col gap-4 items-center bg-red-1300">
+                <img src={aBenefit.image} alt={aBenefit.heading} />
+                <h3>{aBenefit.heading}</h3>
+                <p className="descriptive-text">{aBenefit.description}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Benefits;
