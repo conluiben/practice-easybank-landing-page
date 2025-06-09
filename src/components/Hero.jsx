4b22b76856041549e811424afbc314368b62bcc9
CoreData Research <cdresearch@Luiz---CoreDatas-MacBook-Air.local>
2025-06-09 09:21:33 +0800

    refactor: hero layout
    - refactored hero component for consistent container structure with other components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import imgMockups from "../assets/image-mockups.png";
 const Hero = ({ className }) => {
   return (
     <div
-      className={`flex flex-col flex-col-reverse relative items-center justify-center ${className} min-h-[30vh] xl:min-h-[50vh] bg-neutral-lightgray bg-[url(/assets/bg-intro-mobile.svg)] lg:bg-[url(/assets/bg-intro-desktop.svg)] bg-contain lg:bg-[length:auto_60vw] bg-no-repeat lg:bg-[center_right_-25vw] z-0 gap-4 py-8 px-8 lg:px-16 lg:py-32`}
+      className={`flex relative ${className} min-h-[30vh] xl:min-h-[50vh] bg-neutral-lightgray bg-[url(/assets/bg-intro-mobile.svg)] lg:bg-[url(/assets/bg-intro-desktop.svg)] bg-contain lg:bg-[length:auto_60vw] bg-no-repeat lg:bg-[center_right_-25vw] z-0 py-8 px-8 md:px-16 lg:py-32`}
     >
-      <div className="w-full max-w-[1200px]">
+      <div className="w-full max-w-[1200px] flex flex-col flex-col-reverse items-center lg:items-start gap-4">
         <div className="w-full text-center lg:text-left lg:w-1/3">
-          <h1 className="mb-2 text-4xl mb:text-5xl font-extralight text-neutral-800">
+          <h1 className="mb-2 text-3xl md:text-4xl font-extralight text-neutral-800">
             Next generation digital banking
           </h1>
           <p className="descriptive-text mb-4">
@@ -18,12 +18,12 @@ const Hero = ({ className }) => {
           </p>
           <Button>Request Invite</Button>
         </div>
+        <img
+          src={imgMockups}
+          alt="Phone Mockups"
+          className="lg:absolute lg:top-1/2 lg:-translate-y-1/2 lg:-right-[7.5%] lg:w-[45%] w-[110%] max-w-[100vw] lg:max-w-[100%] lg:max-h-[120vh] -mt-[25%] lg:mt-0 top-0 object-contain z-10"
+        />
       </div>
-      <img
-        src={imgMockups}
-        alt="Phone Mockups"
-        className="lg:absolute lg:top-1/2 lg:-translate-y-1/2 lg:-right-[7.5%] lg:w-[45%] w-[110%] max-w-[100vw] lg:max-w-[100%] lg:max-h-[120vh] -mt-[25%] lg:mt-0 top-0 object-contain z-10"
-      />
     </div>
   );
 };
