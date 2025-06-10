20aa2ec5d07317b85508e412b51cb3850ec18c54
CoreData Research <cdresearch@Luiz---CoreDatas-MacBook-Air.local>
2025-06-10 10:07:15 +0800

    fix: responsive footer
    - fixed footer design for larger screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,19 +28,23 @@ const Footer = () => {
   return (
     <Container
       classNameOuter="bg-darkblue"
-      classNameInner="flex flex-col gap-8"
+      classNameInner="flex flex-col lg:flex-row gap-8 lg:gap-24"
     >
-      <div className="flex flex-col items-center justify-center gap-8">
+      <div className="flex flex-col items-center justify-center lg:items-start gap-8 lg:gap-6">
         <img src={imgLogo} alt="Easybank Logo" className="invert" />
         <div className="flex gap-4">
           {listSocmed.map((aSocmed, idx) => (
             <a href="#" key={idx}>
-              <img src={aSocmed.image} alt={aSocmed.name} />
+              <img
+                src={aSocmed.image}
+                alt={aSocmed.name}
+                className="w-[25px]"
+              />
             </a>
           ))}
         </div>
       </div>
-      <div className="grid grid-cols-1 text-center justify-items-center gap-y-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 text-center justify-items-center lg:justify-items-start gap-4 lg:gap-x-16">
         {menuItems.map((aLink, idx) => (
           <a
             href="#"
@@ -51,7 +55,7 @@ const Footer = () => {
           </a>
         ))}
       </div>
-      <div className="flex flex-col items-center gap-8">
+      <div className="flex flex-col items-center justify-center lg:items-end lg:grow gap-8 lg:gap-4">
         <Button>Request Invite</Button>
         <p className="text-neutral-500">© Easybank. All Rights Reserved.</p>
       </div>
